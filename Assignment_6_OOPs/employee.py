e43bb273413f226060fdb6b865c2bbd09b14f4e0
Rashid Vadekandy Meethel <rashbsf@gmail.com>
2025-01-18 10:23:17 +0400

    Add files via upload

---
+++ Assignment_6_OOPs/employee.py
@@ -0,0 +1,12 @@
+# module:employee.py
+
+class Employee:
+    def __init__(self, name, salary):
+        self._name = name
+        self._salary = salary
+
+    def get_name(self):
+        return self._name
+
+    def get_salary(self):
+        return self._salary
